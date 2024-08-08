0d25f3e388a9600659c556110e4a96c231d5e501
김동균 <wooheuy5752@nate.com>
2024-08-08 19:08:43 +0900

    [alkong] chore : flex 스타일 설정 #3

--- alkong/src/app/page.tsx
+++ alkong/src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <>
-      <p className="text-logo">color</p>
-      <p className="title-B">color</p>
+      <div className="flexBetween">
+        <p className="text-logo">color</p>
+        <p className="title-B">color</p>
+      </div>
       <p>color</p>
       <p>color</p>
     </>
