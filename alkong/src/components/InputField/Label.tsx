9edd881b86a5b08ebb1032cdf708194c431f60e1
김동균 <wooheuy5752@nate.com>
2024-08-11 08:40:21 +0900

    [alkong] chore : LabelWithIcon 이미지 설정
    #9

--- alkong/src/components/InputField/Label.tsx
+++ alkong/src/components/InputField/Label.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image'
+
 import type { ChildrenProps } from '@/types/common'
+
+interface ILabelWithIcon {
+  src: string
+  children: React.ReactNode
+}
 
 export const Label = ({ children }: ChildrenProps) => {
   return <label className="subtitle-B">{children}</label>
 }
 
-export const LabelWithIcon = ({ children }: ChildrenProps) => {
-  return <label className="subtitle-B flexAlign gap-2">{children}</label>
+export const LabelWithIcon = ({ src, children }: ILabelWithIcon) => {
+  return (
+    <label className="flexAlign gap-2">
+      <Image src={src} width={28} height={28} alt="label-icon" />
+      <p className="subtitle-B">{children}</p>
+    </label>
+  )
 }
