d41064bc7852db7744d8be2e8f307835d6a06f8b
김동균 <wooheuy5752@nate.com>
2024-08-11 11:36:54 +0900

    [alkong] chore : TextWithArrow 이벤트 추가
    화살표 클릭 시의 이벤트 내려받기
    
    #9

--- alkong/src/components/InputField/Element.tsx
+++ alkong/src/components/InputField/Element.tsx
@@ -5,14 +5,22 @@ import arrowImg from '/public/image/arrow-right__green.svg'
 interface ITextWithArrow {
   isFull?: boolean
   children: React.ReactNode
+  handleClick: () => void
 }
 
-export const TextWithArrow = ({ isFull = false, children }: ITextWithArrow) => {
+export const TextWithArrow = ({ isFull = false, children, handleClick }: ITextWithArrow) => {
   const flexStyle = isFull ? 'flexBetweenAlign' : 'flexAlign gap-1'
   return (
     <div className={`${flexStyle}`}>
       <p className="body-M text-[#959595]">{children}</p>
-      <Image src={arrowImg} width={28} height={28} className="cursor-pointer" alt="arrow" />
+      <Image
+        src={arrowImg}
+        width={28}
+        height={28}
+        className="cursor-pointer"
+        onClick={handleClick}
+        alt="arrow"
+      />
     </div>
   )
 }
