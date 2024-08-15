9ef4af6e302ca119b307117f0db8590bd5d0f8c7
김동균 <wooheuy5752@nate.com>
2024-08-15 14:38:24 +0900

    [alkong] chore : 몸무게 슬라이더 상수 선언
    #10

--- alkong/src/constants/slider.ts
+++ alkong/src/constants/slider.ts
@@ -1,3 +1,6 @@
 export const HOURS = Array.from({ length: 12 }, (_, i) => String(i + 1).padStart(2, '0'))
 export const MINUTES = Array.from({ length: 60 }, (_, i) => String(i).padStart(2, '0'))
 export const TIME_PERIOD = ['AM', 'PM']
+
+export const INTEGER_WEIGHT = Array.from({ length: 99 }, (_, i) => String(i + 1).padStart(2, '0'))
+export const DECIMAL_WEIGHT = Array.from({ length: 10 }, (_, i) => String(i))
