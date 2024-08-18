288d1135421868f1fa8603847e30ddaa20996a2b
김동균 <wooheuy5752@nate.com>
2024-08-18 17:57:59 +0900

    [shop] chore : firebase 설정
    #17

---
+++ shop/src/firebase/firebase.js
@@ -0,0 +1,26 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: NEXT_PUBLIC_STORAGE_BUCHET,
+  messagingSenderId: NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: NEXT_PUBLIC_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFireStore(app);
+export const storage = getStorage(app);
+
+export default app;
