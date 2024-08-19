4e1102a16be7f6e5786c1b180413c1b273b1aadf
김동균 <wooheuy5752@nate.com>
2024-08-19 12:27:15 +0900

    [alkong] refactor : Toggle 로직 변경
    zustand의 store를 사용하는 방식이 아닌 커스텀 훅으로 상태를 변경하는 방식 사용
    onClick에서 이전 상태를 사용할 수 있도록 변경

--- alkong/src/components/Toggle.tsx
+++ alkong/src/components/Toggle.tsx
@@ -1,41 +1,27 @@
-'use client'
+import { useToggle } from '@/hooks/useToggle'
 
-import { useEffect } from 'react'
-
-import { useToggleActions, useToggleState } from '@/store/toggle'
-
-interface IToggle {
+interface Props {
   initial?: boolean
-  onClick: () => void
+  onClick: (prev: boolean) => void
 }
 
-const Toggle = ({ initial = false, onClick }: IToggle) => {
-  const isActive = useToggleState()
-  const { setInitialState, changeIsActive } = useToggleActions()
-  const handleToggleClick = () => {
-    onClick()
-    changeIsActive()
-  }
+const Toggle = ({ initial = false, onClick }: Props) => {
+  const [isActive, changeToggleState] = useToggle(initial)
 
-  useEffect(() => {
-    setInitialState(initial)
-  }, [initial, setInitialState])
+  const handleToggleClick = () => {
+    onClick(isActive)
+    changeToggleState()
+  }
 
   const containerStyle = isActive ? 'bg-mint-6' : 'bg-gray-2'
   const innerStyle = isActive ? 'translate-x-8 bg-white' : 'translate-x-0 bg-mint-6'
 
   return (
     <div className="inline-block">
-      <input
-        type="checkbox"
-        id="toggle"
-        name="toggle"
-        onChange={handleToggleClick}
-        className="hidden"
-      />
+      <input type="checkbox" id="toggle" onChange={handleToggleClick} className="hidden" />
       <label
         htmlFor="toggle"
-        className={`flex-align h-[34px] w-[66px] shrink-0 rounded-full p-[5px] transition-all focus-visible:outline-none ${containerStyle}`}
+        className={`flex-align h-[34px] w-[66px] shrink-0 cursor-pointer rounded-full p-[5px] transition-all focus-visible:outline-none ${containerStyle}`}
       >
         <div className={`size-6 rounded-full transition-all ${innerStyle}`}>{''}</div>
       </label>
