10924d9d173b9e225f77ac843bb67d36c61db4f9
김동균 <wooheuy5752@nate.com>
2024-08-30 16:39:58 +0900

    [alkong] chore : tailwind 파일 위치 feature 폴더 추가
    #20

--- alkong/tailwind.config.ts
+++ alkong/tailwind.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/feature/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontWeight,
@@ -28,6 +29,7 @@ const config: Config = {
       boxShadow,
     },
   },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require('tailwind-scrollbar-hide')],
 }
 export default config
