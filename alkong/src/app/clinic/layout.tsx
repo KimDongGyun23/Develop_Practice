6f2e06888f8ad8a93a189572115dde6d578156a6
김동균 <wooheuy5752@nate.com>
2024-08-30 16:40:22 +0900

    [alkong] feat : layout 스타일 수정
    #20

--- alkong/src/app/clinic/layout.tsx
+++ alkong/src/app/clinic/layout.tsx
@@ -6,12 +6,12 @@ export const ClinicLayout = ({ children }: PropsWithChildren) => {
   const name = '김동균'
 
   return (
-    <section className="px-5">
+    <main className="min-h-full px-5 scrollbar-hide">
       <div className="absolute right-5 top-[22px]">
         <Profile name={name} size="sm" bgColor="#C5FDEC" />
       </div>
       {children}
-    </section>
+    </main>
   )
 }
 
