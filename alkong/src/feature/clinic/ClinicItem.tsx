9721ab11471908bd6f08b437fbff7bc2b680c783
김동균 <wooheuy5752@nate.com>
2024-09-01 17:15:58 +0900

    [alkong] feat : 진료 정보 페이지 UI 완성
    #20

--- alkong/src/feature/clinic/ClinicItem.tsx
+++ alkong/src/feature/clinic/ClinicItem.tsx
@@ -1,26 +1,34 @@
 'use client'
 
+import dayjs from 'dayjs'
+
 import { Icon, Tag } from '@/components'
 import type { ClinicCalendarType } from '@/types'
+
+import 'dayjs/locale/ko'
+
+dayjs.locale('ko')
 
 const ClinicItem = ({
   hospitalName,
   hospitalDate,
   medicalPart,
 }: Omit<ClinicCalendarType, 'medicalId'>) => {
+  const time = dayjs(hospitalDate).format('HH:mm')
+  const date = dayjs(hospitalDate).format('M월 DD일(dd) A HH:mm')
   return (
     <section className="rounded-xl bg-mint-0 p-4">
       <div className="flex-between">
         <div className="flex-col">
           <p className="subtitle-B">
-            {hospitalDate} {hospitalName}
+            {time} {hospitalName}
           </p>
-          <p className="headline-M text-gray-6">{hospitalDate}</p>
+          <p className="headline-M text-gray-6">{date}</p>
         </div>
         <Icon name="check-no" size={36} />
       </div>
 
-      <div className="mt-8">
+      <div className="mt-8 flex flex-wrap gap-2">
         {medicalPart.map((part) => (
           <Tag key={part}>{part}</Tag>
         ))}
