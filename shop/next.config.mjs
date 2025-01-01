f9d0bd26898dada6a4d4576511ab054354395ccf
DongGyun <wooheuy5752@gmail.com>
2025-01-01 11:57:10 +0900

    feat: Slider 컴포넌트 구현

--- shop/next.config.mjs
+++ shop/next.config.mjs
@@ -10,6 +10,9 @@ const nextConfig = {
     config.resolve.alias["@"] = path.join(__dirname, "src");
     return config;
   },
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
--- shop/src/app/(home)/page.jsx
+++ shop/src/app/(home)/page.jsx
@@ -1,5 +1,13 @@
 import styles from "../page.module.css";
 
+import { Slider } from "@/components/slider/Slider";
+import { Product } from "@/components/product/Product";
+
 export default function Home() {
-  return <main className={styles.main}>home</main>;
+  return (
+    <>
+      <Slider />
+      <Product />
+    </>
+  );
 }
